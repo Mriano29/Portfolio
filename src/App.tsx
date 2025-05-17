1ff9b9923c8b59390a4b2e9d7572cf32a817b1f6
Mriano29 <mriano290801@gmail.com>
2025-05-17 22:49:45 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <main className="flex h-screen w-full items-center justify-center bg-fuchsia-900">
+      <h1 className="text-5xl">this is a test with vite and tailwind</h1>
+    </main>
+  );
+}
